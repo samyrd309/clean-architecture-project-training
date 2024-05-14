1f0f6d8169a3129d19774139b4238b8849a0064e
Samuel Charles <samyrd309@gmail.com>
2024-05-14 13:52:15 -0400

    Packages instalation

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -2,6 +2,7 @@ namespace Domain.Entities
 {
     public class User 
     {
+        public int Id { get; set; }
         public string ? Name { get; set; }
         public string ? Email { get; set; }
         public string ? Password { get; set; }
--- src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784d3533e6ae9027ecc5e3090fafb8644c21f3bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
