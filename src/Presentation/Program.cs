1ba92de7dbdc2d6a62ba0d846cdfe00f4a149b4f
Samuel Charles <samyrd309@gmail.com>
2024-05-14 14:33:31 -0400

    Dependencie configurations

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,5 +1,15 @@
+using CleanArchitectureImplementation.Application;
+using CleanArchitectureImplementation.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add layer depencies
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
