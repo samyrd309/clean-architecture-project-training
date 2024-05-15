2666f8ed2e292b9ebed15379aebcf65e974bbd74
Samuel Charles <samyrd309@gmail.com>
2024-05-15 15:14:47 -0400

    AppDbContext

---
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+namespace CleanArchitectureImplementation.Infrastructure.Data;
+
+using CleanArchitectureImplementation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    
+}
